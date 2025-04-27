878039ce2e930950170d40434b56e6ae076c92b7
Ignacio Andrés Ávila Moreno <ignacio.avila@usach.cl>
2025-04-27 18:33:28 -0400

    Create Reports.jsx

---
+++ KartingRMFront/karting-frontend/src/components/Reports.jsx
@@ -0,0 +1,11 @@
+// src/components/Reports.jsx
+import React from 'react';
+
+export default function Reports() {
+  return (
+    <div className="container mt-4">
+      <h2>Reportes</h2>
+      <p>Aquí irán los reportes en una futura iteración.</p>
+    </div>
+  );
+}
