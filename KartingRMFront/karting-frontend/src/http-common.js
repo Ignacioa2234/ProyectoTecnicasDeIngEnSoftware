6b3585078285a29e8cac600576426880d70f1df7
Ignacio Andrés Ávila Moreno <ignacio.avila@usach.cl>
2025-04-27 23:50:54 -0400

    Update http.common.js

--- KartingRMFront/karting-frontend/src/http-common.js
+++ KartingRMFront/karting-frontend/src/http-common.js
@@ -1,6 +1,9 @@
+// src/http-common.js
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: 'http://4.201.152.46:8010/api',
+  headers: {
+    'Content-type': 'application/json'
+  }
 });
